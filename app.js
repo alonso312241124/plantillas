5592b7f15fbd7e1d7ed92993e9676f10ea3796a7
Alonso <alonsolopezm1234@gmail.com>
2025-01-21 16:09:41 +0000

    v1.1

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ app.set('view engine','ejs')
 app.get('/', function (req, res) {
     const datos = [
         {nombre: 'Ana', apellidos: 'García'},
-        {nombre: 'Juan', apellidos: 'López'}        
+        {nombre: 'Juan', apellidos: 'López'}
     ]
     res.render('index', {datos:datos});
+})
+
+app.get('/saludo', function (req, res) {
+    let hora = new Date();
+    res.render('saludo', {hora:hora.getHours()});
 })
 
 app.listen(3000)
